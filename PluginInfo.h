f42e8ddddebee7e70d427b605fd56af5329b26ea
Rogelio Guzman <rogguzma@cisco.com>
2019-05-20 09:15:03 -0500

    Corrected plugin info

--- PluginInfo.h
+++ PluginInfo.h
@@ -12,14 +12,14 @@
 // Description:  Fundamental definitions for this project.
 //
 ///////////////////////////////////////////////////////////////////////////////
+/*
 #define PLUGIN_NAME     _T("aPausemachine")
 #define PLUGIN_VERSION  _T("1.6")
 #define PLUGIN_GUID     _T("{F71AAE41-82E7-4A54-A533-536473B3AF9D}")
 #define PLUGIN_PORT     _T("4004")
+*/
 
-/*
 #define PLUGIN_NAME     _T("CiscoOpenApp")
 #define PLUGIN_VERSION  _T("1.0")
 #define PLUGIN_GUID     _T("{0CE79B4B-8CA4-4097-AF5B-7D2BCE044F2E}")
 #define PLUGIN_PORT     _T("4004")
-*/

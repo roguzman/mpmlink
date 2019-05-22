58af0828a6ca163e012e4edbd3bbe5abae4bddf4
Rogelio Guzman <roguzman>
2019-05-21 20:17:17 -0500

    Added new features

--- PluginRpcCalls.cpp
+++ PluginRpcCalls.cpp
@@ -31,8 +31,13 @@ CString text;
 
 void UpdateCEdit(LPCTSTR text)
 {
-	strBuffer.Append(text);
-	m_CEditMessage.SetWindowTextW(strBuffer);
+	int nLength = m_CEditMessage.GetWindowTextLength();
+	// put the selection at the end of text
+	m_CEditMessage.SetSel(nLength, nLength);
+	// replace the selection
+	m_CEditMessage.ReplaceSel(text);
+	// strBuffer.Append(text);
+	// m_CEditMessage.SetWindowTextW(strBuffer);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -57,7 +62,7 @@ void RpcPIGetNotificationList( handle_t hdl, hyper i64Timestamp, short* sListLen
     // for this simple example anything non-NULL will work.
     // *pphContext = (PCONTEXT_HDL_TYPE)0x00000001;
 	*pphContext = (PCONTEXT_HDL_TYPE)pContextHandler;
-	OutputDebugStringW(L"RpcPIGetNotificationList\n");
+	UpdateCEdit(L"Subscribed to : [ePINotifyTypeMessage, ePINotifyTypeBoard, ePINotifyTypeState]\r\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -77,7 +82,6 @@ void RpcPIGetVersionInfo( handle_t hdl, hyper i64Timestamp, short* sNameLen, uns
 
     _sntprintf_s( (wchar_t*)usSoftwareVersion, *sSoftwareVersionLen, _TRUNCATE, _T("%s"), PLUGIN_VERSION );
     *sSoftwareVersionLen = _tcslen( (wchar_t*)usSoftwareVersion ) + 1;
-	OutputDebugStringW(L"RpcPIGetVersionInfo\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -91,8 +95,6 @@ void RpcPIGetVersionInfo( handle_t hdl, hyper i64Timestamp, short* sNameLen, uns
 void RpcPIPing( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp )
 {
     // do nothing
-	OutputDebugStringW(L"RpcPIPing\n");
-
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -106,7 +108,6 @@ void RpcPIPing( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp )
 void RpcPIDisconnect( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp )
 {
     // do nothing
-	OutputDebugStringW(L"RpcPIDisconnect\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -115,19 +116,16 @@ void RpcPIDisconnect( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp )
 void RpcPIAlarmChange( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lAlarmId, EPIAlarmAction eAction )
 {
 	// do nothing
-	OutputDebugStringW(L"RpcPIAlarmChange\n");
 }
 
 void RpcPIAlarmSysStateChange (PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, EPIAlarmSysState eNewState )
 {
 	// do nothing
-	OutputDebugStringW(L"RpcPIAlarmSysStateChange\n");
 }
 
 void RpcPIAlarmTriggered( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lAlarmId, EPIAlarmType eAlarmType, EPIAlarmSubType eAlarmSubType, EPIAlarmSeverity eSeverity, short sTextLen, unsigned short usText[], short sPluginBindingLen, unsigned short usPluginBinding[], short sPluginNameLen, unsigned short usPluginName[], hyper i64UserData )
 {
 	// do nothing
-	OutputDebugStringW(L"RpcPIAlarmTriggered\n");
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -135,7 +133,7 @@ void RpcPIAlarmTriggered( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long 
 ///////////////////////////////////////////////////////////////////////////////
 void RpcPIPostMessage( PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, short sMessageLen, unsigned short usMessage[] )
 {
-	text.Format(_T("- RpcPIPostMessage : %s \r\n"), (wchar_t*)usMessage);
+	text.Format(_T("Message sent by Benchmark : Timestamp=%I64d Message=%s \r\n"), i64Timestamp, usMessage);
 	UpdateCEdit(text);
 }
 
@@ -150,12 +148,12 @@ void RpcPIDiagnosticsModeEnded(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp) 
 void RpcPIDiagnosticsModeStarted(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp) {}
 void RpcPIDispenseComplete(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp)
 {
-	text.Format(_T("Dispense Completed : %d \r\n"), i64Timestamp);
+	text.Format(_T("Dispense Completed : TimeStamp=%I64d \r\n"), i64Timestamp);
 	UpdateCEdit(text);
 }
 void RpcPIDispenseStarted(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, short sProfileNameLen, unsigned short usProfileName[])
 {
-	text.Format(_T("Dispense Started : %d %s \r\n"), i64Timestamp, (wchar_t*)usProfileName);
+	text.Format(_T("Dispense Started : TimeStamp=%I64d  ProfileName=%s \r\n"), i64Timestamp, usProfileName);
 	UpdateCEdit(text);
 }
 void RpcPIKneadNotification(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId) {}
@@ -179,7 +177,48 @@ void RpcPIWipeStartedWithData(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, s
 	EPIWipePaperFeedMode ePaperFeedMode, EPIWipeEventType eWipeEventType, short sWipeFrequency, short sNumSegments, double dSegmentIndexDistanceMm[], double dSegmentSpeedMmPerSec[], 
 	EPIWipeStrokeType eStrokeType[])
 {
-	text.Format(_T("Wipe Started : %I64d, %s, %d, %d, %d, %s\r\n"), i64Timestamp, usProfileName, ePaperFeedMode, eWipeEventType, sWipeFrequency, eStrokeType);
+	CString strTemp;
+
+	strTemp.Append(L"[");
+	for (int i = 0; i < sNumSegments; i++)
+	{
+		switch (eStrokeType[i])
+		{
+		case ePIWipeStrokeTypeDryR2F:
+			strTemp.Append(L"DRY-R2F, ");
+			break;
+		case ePIWipeStrokeTypeDryF2R:
+			strTemp.Append(L"DRY-F2R, ");
+			break;
+		case ePIWipeStrokeTypeSolventR2F:
+			strTemp.Append(L"SOLVENT-R2F, ");
+			break;
+		case ePIWipeStrokeTypeSolventF2R:
+			strTemp.Append(L"SOLVENT-F2R, ");
+			break;
+		case ePIWipeStrokeTypeVacuumR2F:
+			strTemp.Append(L"VACUUM-R2F, ");
+			break;
+		case ePIWipeStrokeTypeVacuumF2R:
+			strTemp.Append(L"VACUUM-R2F, ");
+			break;
+		case ePIWipeStrokeTypeVacuumSolventF2R:
+			strTemp.Append(L"SOLVENT-F2R, ");
+			break;
+		case ePIWipeStrokeTypeVacuumSolventR2F:
+			strTemp.Append(L"SOLVENT-R2F, ");
+			break;
+		case ePIWipeStrokeTypeLast:
+		default:
+			strTemp.Append(L"UNKNOWN");
+			break;
+		}
+	}
+	strTemp.Delete(strTemp.GetLength() - 2, 2);
+	strTemp.Append(L"]");
+
+
+	text.Format(_T("Wipe Started : TimeStamp=%I64d  Profilename=%s  PapedFeedMode=%d  WipeEventType=%d  WipeFrequency=%d  SrtokeType=%s\r\n"), i64Timestamp, usProfileName, ePaperFeedMode, eWipeEventType, sWipeFrequency, (LPCWSTR)strTemp);
 	UpdateCEdit(text);
 }
 
@@ -191,7 +230,24 @@ void RpcPIBoardBarcodeNotification(PCONTEXT_HDL_TYPE phContext, hyper i64Timesta
 void RpcPIBoardClamped(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId) { OutputDebugStringW(L"RpcPIBoardClamped\n"); }
 void RpcPIBoardPrinted(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardNumber, short sBarcodeLen, unsigned short usBarcode[], boolean bInitStrokeF2R, short sNumStrokes, EPIProcessedStatus eProcessedStatus, long lPackagedDataLength, byte packagedData[]) 
 { 
-	text.Format(L"{\"timestamp\" : %I64d, \"board_printed\" : %d, \"barcode\" : \"%s\", \"status\": %d}\r\n", i64Timestamp, lBoardNumber, usBarcode, eProcessedStatus);
+	LPCWSTR sResult = new TCHAR[80];
+
+	switch (eProcessedStatus)
+	{
+	case ePIProcessedStatusAOK:
+		sResult = L"OK";
+		break;
+	case ePIProcessedStatusError:
+		sResult = L"ERROR";
+		break;
+	case ePIProcessedStatusLast:
+		sResult = L"LAST";
+		break;
+	default:
+		sResult = L"UNKNOWN";
+		break;
+	}
+	text.Format(L"Board Printed : TimeStamp=%I64d  BoardNumber=%d  BarCode=%s  NumStrokes=%d  Status=%s \r\n", i64Timestamp, lBoardNumber, usBarcode, sNumStrokes, sResult);
 	UpdateCEdit(text); 
 }
 void RpcPIBoardProcgCompleted(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId, long lBoardCount) {}
@@ -202,12 +258,12 @@ void RpcPIClearQueue(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp) {}
 void RpcPIPostPanelData(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId, short sNumPanels, short sPanelIdList[], unsigned short usBarcodeList[][BENCHMARK_PII_MAX_BARCODE_LEN]) {}
 void RpcPIPostPasteHeightData(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId, double dPasteHeightMinMm, double dPasteHeightMeasuredMm)
 {
-	text.Format(_T("Paste Height Data : %d, %d, %f, %f \r\n"), i64Timestamp, lBoardId, dPasteHeightMinMm, dPasteHeightMeasuredMm);
+	text.Format(_T("Paste Height Data :  TimeStamp=%I64d  BoardId=%d  PasteHeightMinMm=%f  PasteHeightMeasuredMm=%f \r\n"), i64Timestamp, lBoardId, dPasteHeightMinMm, dPasteHeightMeasuredMm);
 	UpdateCEdit(text);
 }
 void RpcPIPostPasteHeightDataEx(PCONTEXT_HDL_TYPE phContext, hyper i64Timestamp, long lBoardId, double dPasteHeightMinMm, double dPasteHeightMaxMm, double dPasteHeightMeasuredMm)
 {
-	text.Format(_T("Paste Height Data : %d, %d, %f, %f, %f \r\n"), i64Timestamp, lBoardId, dPasteHeightMinMm, dPasteHeightMaxMm, dPasteHeightMeasuredMm);
+	text.Format(_T("Paste Height Data :  TimeStamp=%I64d BoardId=%d  PasteHeightMinMm=%f  PasteHeightMaxMm=%f  PasteHeightMeasuredMm=%f \r\n"), i64Timestamp, lBoardId, dPasteHeightMinMm, dPasteHeightMaxMm, dPasteHeightMeasuredMm);
 	UpdateCEdit(text);
 }
 
